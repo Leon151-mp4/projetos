9dde35bee15d41554cb27972a7225c176fb9bed9
Leon151-mp4 <damianafreitasf@gmail.com>
2026-07-16 13:26:26 -0300

    IFOOD

---
+++ projetos js,css,html/Previsão do tempo/config.js
@@ -0,0 +1,12 @@
+//pegando as coisas com const
+const key = "2de5d9e48284e2d449eec105f40ab21f"
+
+const dtemp = document.querySelector('lht');
+const stats = document.querySelector('stats');
+
+
+
+
+function voando(cidade) {
+    const urlAPI = "https://home.openweathermap.org/"
+}
